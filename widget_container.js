b12a58cc22324ef6b13563f23f900f60a14ae7e8
thejasenapps <enapps07@gmail.com>
2026-03-09 11:00:48 +0530

    Update widget_container.js

--- widget_container.js
+++ widget_container.js
@@ -12,8 +12,8 @@
       :host { all: initial; }
 
       #container{
-        width:100%;
-        height:100%;
+        width:400px;
+        height:700px;
         background:white;
         border-radius:15px;
         box-shadow:0 10px 30px rgba(0,0,0,0.3);
